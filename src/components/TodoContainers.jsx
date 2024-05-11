32cea5a8e811be6389ab38f1949d4b9d35893b6a
NeilDogii <aryaworrior41@gmail.com>
2024-05-12 04:35:00 +0530

    adding drag

--- src/components/TodoContainers.jsx
+++ src/components/TodoContainers.jsx
@@ -1,10 +1,21 @@
 import "../global-css/todoContainer.css";
+import Draggable from 'react-draggable'
+import {Helmet} from "react-helmet";
 function TodoContainer() {
   return (
-    <div className="TodoContainer">
+    <>
+    <Helmet>
+      <script src="../global-js/Tododrag" type="text/babel" 
+      crossorigin="anonymous"
+      async
+      />
+      
+    </Helmet>
+    <Draggable  handle=".TodoContainer" bound="parent">
+    <div className="TodoContainer" id="TodoMain">
       <div className="status">
         <div className="status_logo">
-          <img src="./done.svg" alt="checked" />
+          <img src="./done.svg"  alt="checked" /> 
         </div>
         <div className="status_text">
           <p>Completed</p>
@@ -16,6 +27,8 @@ function TodoContainer() {
         </div>
       </div>
     </div>
+    </Draggable>
+    </>
   );
 }
 
