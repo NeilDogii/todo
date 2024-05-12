384c6aed0ccee5655169e90649c255bfcb5d0a55
NeilDogii <aryaworrior41@gmail.com>
2024-05-13 03:02:33 +0530

    adding resize and scroll styling, and adding the buttons

--- src/components/TodoContainers.jsx
+++ src/components/TodoContainers.jsx
@@ -1,33 +1,43 @@
 import "../global-css/todoContainer.css";
 import Draggable from 'react-draggable'
-import {Helmet} from "react-helmet";
 function TodoContainer() {
   return (
     <>
-    <Helmet>
-      <script src="../global-js/Tododrag" type="text/babel" 
-      crossorigin="anonymous"
-      async
-      />
-      
-    </Helmet>
-    <Draggable  handle=".TodoContainer" bound="parent">
-    <div className="TodoContainer" id="TodoMain">
-      <div className="status">
-        <div className="status_logo">
-          <img src="./done.svg"  alt="checked" /> 
+      <Draggable handle=".status" bounds={{ top: 10, left: 0 }}>
+        <div className="TodoContainer" id="TodoMain">
+          <div className="status">
+            <div className="status_logo">
+              <img src="./done.svg" alt="checked" />
+            </div>
+            <div className="status_text">
+              <p>Completed</p>
+            </div>
+          </div>
+          <div className="status_subcontainer">
+            <div className="status_topic">
+              <textarea placeholder="topic..." />
+            </div>
+          </div>
+          <div className="status_button_container">
+            <div className="status_types">
+              <select>
+
+                <option value="fruit">Fruit</option>
+
+                <option value="vegetable">Vegetable</option>
+
+                <option value="meat">Meat</option>
+
+              </select>
+            </div>
+            <div className="todoclose">
+              <button>
+                <img src="./cross.svg" alt="close" />
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="status_text">
-          <p>Completed</p>
-        </div>
-      </div>
-      <div className="status_subcontainer">
-        <div className="status_topic">
-          <textarea placeholder="topic..."/>
-        </div>
-      </div>
-    </div>
-    </Draggable>
+      </Draggable>
     </>
   );
 }
